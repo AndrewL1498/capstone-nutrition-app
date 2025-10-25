601e2c2c328df2371aaaf17ad39ecff297f34c7b
AndrewL1498 <andrewlindsey498@gmail.com>
2025-10-25 13:02:07 -0500

    finished setting up mailer

--- src/app/api/users/signupRoute/route.ts
+++ src/app/api/users/signupRoute/route.ts
@@ -2,6 +2,7 @@ import {connect} from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import {NextRequest, NextResponse} from "next/server";
 import bcrypt from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer"
  
 
 connect() //Establishes a connection to the database using the connect function from dbConfig
@@ -33,6 +34,11 @@ export async function POST(request: NextRequest) {
 
         const savedUser = await newUser.save()
         console.log("Saved User:", savedUser);
+
+        //send verfication email
+        await sendEmail({email, emailType: "VERIFY",
+            userId: savedUser._id
+        })
 
         return NextResponse.json({ 
             message: "User created successfully", 
